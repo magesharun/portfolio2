b9946f1b024631b4e5fd7439ac594a0a2dfe4053
{magesharun} <{magesharun2016@gmail.com}>
2024-11-10 22:02:08 +0530

    first-commit

--- Desktop/javascriptproject/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('mode-toggle').addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-  const icon = document.getElementById('mode-toggle').firstElementChild;
-  icon.classList.toggle('bx-sun');
-  icon.classList.toggle('bx-moon');
-});
